ba5e60a28ddf32cbda20239ef1c631d672cf851f
RokasGu <131803858+RokasGu@users.noreply.github.com>
2024-01-25 12:33:04 +0200

    Add a way to choose the winner

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 function getComputerChoice(choices) {
     let random = Math.floor(Math.random() * choices.length);
     let choice = choices[random];
@@ -7,3 +7,20 @@ function getComputerChoice(choices) {
 
 const computerSelection = getComputerChoice(choices);
 console.log(computerSelection);
+
+const playerSelection = prompt("Make your move");
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie! Play again!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return (`You win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+        return (`Computer wins! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
